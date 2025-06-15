53bbee1f87333699eafa2d5f872130589b755153
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2025-06-15 17:08:08 +0200

    Partial implementation

--- src/main/java/it/unibo/data/Piatto.java
+++ src/main/java/it/unibo/data/Piatto.java
@@ -66,5 +66,46 @@ public class Piatto {
                 throw new DAOException("Errore durante il caricamento dei piatti", e);
             }
         }
+        /**
+         * Restituisce gli ordini che contengono un dato piatto
+         */
+        public static List<String> ordiniByPiatto(Connection connection, int codicePiatto) {
+            List<String> result = new ArrayList<>();
+            try (var stmt = DAOUtils.prepare(connection, Queries.ORDINI_BY_PIATTO, codicePiatto);
+                 var rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    result.add(Printer.stringify("Ordine", List.of(
+                        Printer.field("codice_ordine", rs.getInt("codice_ordine")),
+                        Printer.field("codice_pagamento", rs.getInt("codice_pagamento")),
+                        Printer.field("codice_stato", rs.getInt("codice_stato")),
+                        Printer.field("prezzo_totale", rs.getBigDecimal("prezzo_totale")),
+                        Printer.field("piva", rs.getString("piva"))
+                    )));
+                }
+            } catch (Exception e) {
+                throw new DAOException("Errore durante il caricamento degli ordini per piatto " + codicePiatto, e);
+            }
+            return result;
+        }
+
+        /**
+         * Restituisce la classifica dei 10 piatti più ordinati in assoluto
+         */
+        public static List<String> top10Piatti(Connection connection) {
+            List<String> result = new ArrayList<>();
+            try (var stmt = DAOUtils.prepare(connection, Queries.TOP10_PIATTI);
+                 var rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    result.add(Printer.stringify("PiattoTop", List.of(
+                        Printer.field("codice_piatto", rs.getInt("codice_piatto")),
+                        Printer.field("nome", rs.getString("nome")),
+                        Printer.field("totale_ordini", rs.getInt("totale_orari"))
+                    )));
+                }
+            } catch (Exception e) {
+                throw new DAOException("Errore durante il caricamento della classifica piatti", e);
+            }
+            return result;
+        }
     }
 }
